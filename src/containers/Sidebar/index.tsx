8e6d7e7326e8eebcda552be401d7fd170c15dfcd
Guilherme <alvesguilherme148117@gmail.com>
2024-06-19 22:27:04 -0300

    Criando tema e arrumando a responsividade

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -4,18 +4,22 @@ import Paragrafo from '../../components/Paragrafo'
 
 import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
-const Sidebar = () => (
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
       <Avatar />
       <Titulo fontSize={20}>Guilherme Alves</Titulo>
       <Paragrafo tipo="secundario" fontSize={16}>
-        GuilhermeAlves148
+        @GuilhermeAlves148
       </Paragrafo>
       <Descricao tipo="principal" fontSize={12}>
         Engenheiro Front-End.
       </Descricao>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
   </aside>
 )
